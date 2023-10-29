f29e977cf3157fdce2cea4226f2bd65879badf0e
Dmitrii Valuev <dima.vap2013@gmail.com>
2023-10-29 16:31:41 +0200

    Initial Commit

---
+++ VisionGestures/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VisionGestures
+//
+//  Created by Dmitrii on 29.10.2023.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ VisionGestures/VisionGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionGesturesApp.swift
+//  VisionGestures
+//
+//  Created by Dmitrii on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct VisionGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
